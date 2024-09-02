79bf42a51b2b255fadeede47483f74ae096ae21e
Kalin Petkov <kalinpetkov314@gmail.com>
2024-09-02 18:57:09 +0300

    Added ShotGrid version id field an more validation to `Motion`

--- cairos_types/core.py
+++ cairos_types/core.py
@@ -1,38 +1,48 @@
 from pydantic.v1 import BaseModel, root_validator
-
+import os
 
 class Motion(BaseModel):
+    sg_id: int
     action: str
     filepath: str
     tags: str | list[str]
 
     @root_validator
     def check_motion(cls, values):
-        import os
+        sg_id = values.get('sg_id')
+        action = values.get('action')
+        filepath = values.get('filepath')
+        tags = values.get('tags')
 
-        action = values.get("action")
-        filepath = values.get("filepath")
-        tags = values.get("tags")
+        # since a ShotGrid version always has an ID this is probably the least
+        # probable case. Therefore we can use the sg_id to identify the motion
+        # in the error messages
+        if sg_id is None:
+            if not isinstance(sg_id, int):
+                raise ValueError(f'Motion with id {sg_id} should have an integer id.')
+        else:
+            raise ValueError(f'Motion missing id.')
+
+        if action is None or len(action) == 0:
+            raise ValueError(f'Motion with id {sg_id} has missing or empty name.')
+
         if not os.path.isfile(filepath):
-            raise ValueError(f"Motion {action} cannot be found at path {filepath}")
-        return values
+            raise ValueError(f'Motion with id {sg_id} cannot be found at path {filepath}')
 
+        return values
 
 class Motions(BaseModel):
     key: str
     # tool_call_id: str
     entries: list[Motion]
 
-
 class Animation(BaseModel):
     sequence: list[Motion]
     description: str
 
-
 class MockMotion(BaseModel):
     action: str
     description: str
-
 
 class MockMotions(BaseModel):
     motions: list[MockMotion]
