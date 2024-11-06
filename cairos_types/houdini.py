055c38b9333712dd6e2a9f6782caaf3f906a511c
Kalin Petkov <kalinpetkov314@gmail.com>
2024-11-06 13:28:41 +0200

    Simplified existing types, added ones for avatar ingest job

--- cairos_types/houdini.py
+++ cairos_types/houdini.py
@@ -3,38 +3,42 @@ from typing import List
 from pydantic.v1 import BaseModel, BaseSettings
 from uuid import UUID
 
-class ExportMotionsRequest(BaseModel):
-    scene_path: Path
-    input_top_node_path: str
-    data_input_node: str
-    user_def_data_key: str
-    output_node_path: str
-    output_exec_parm_name: str
-    job_id: tuple[str, UUID]
-    motions: List
-    output_path: Path
-    avatar: str | None
-    avatar_node_path: str
-    avatar_parm_name: str
+class BaseHoudiniConfig(BaseSettings):
+    server_port: int = 18861
+    server_host: str = "cairos-houdini-server"
 
+class HoudiniError(BaseModel):
+    error_message: str
 
-class CairosHoudiniConfig(BaseSettings):
+class SequencerConfig(BaseHoudiniConfig):
     input_top_node_path: str = "/obj/geo_sequence_clips/topnet1"
     data_input_node: str = "/obj/geo_sequence_clips/input_data"
     user_def_data_key: str = "motions"
-
     output_node_path: str = "/obj/ropnet/char-gltf"
     output_exec_parm_name: str = "execute"
     avatar_node_path: str = "/obj/geo_avatar_rest/file2"
     avatar_parm_name: str = "file"
-    server_port: int = 18861
-    server_host: str = "cairos-houdini-server"
 
+class SequencerRequest(BaseModel):
+    scene_path: Path
+    job_id: tuple[str, UUID]
+    motions: List
+    output_path: Path
+    avatar: str | None
+    config: SequencerConfig
 
-class CairosHoudiniSuccess(BaseModel):
+class SequencerSuccess(BaseModel):
     job_id: tuple[str, UUID]
     gltf_path: Path
 
+class AvatarIngestConfig(BaseHoudiniConfig):
+    data_input_node: str = "/obj/character/RPC_DATA_COMES_HERE"
+    user_def_data_key: str = "io_paths"
+    render_top_node: str = "/obj/output"
 
-class CairosHoudiniError(BaseModel):
-    error_message: str
+class AvatarIngestRequest(BaseModel):
+    scene_path: Path
+    config: AvatarIngestConfig
+
+class AvatarIngestSuccess(BaseModel):
+    path: Path
