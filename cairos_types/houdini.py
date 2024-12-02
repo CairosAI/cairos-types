e5d55ad281a0726633ecb1c3a7c2285391075094
Kalin Petkov <kalinpetkov314@gmail.com>
2024-12-02 17:51:45 +0200

    Added filepath interpolation in success types with `pre` validators

--- cairos_types/houdini.py
+++ cairos_types/houdini.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Literal, TypeAlias
-from pydantic.v1 import BaseModel, BaseSettings, root_validator
+from pydantic.v1 import BaseModel, BaseSettings, root_validator, validator, Field
 from uuid import UUID
 
 class BaseHoudiniConfig(BaseSettings):
@@ -31,16 +31,32 @@ class SequencerRequest(BaseModel):
 
 class SequencerSuccess(BaseModel):
     request: SequencerRequest
-    fbx: Path | None
-    gltf: Path | None
+    # these fields will be computed from the info in request...
+    fbx: Path | None = Field(...)
+    gltf: Path | None = Field(...)
 
+    # ...using a `pre=True` root_validator
     @root_validator(pre=True)
+    def interp_paths(cls, values):
+        try:
+            values.update({'fbx': Path(values['request'].data.avatar.output, 'output.fbx'),
+                           'gltf': Path(values['request'].data.avatar.output,
+                                        'output-gltf-char.glb')
+                           })
+        except Exception as e:
+            raise e
+        return values
+
+    # Finally we check if they are existing files on mothership3. The type
+    # annotations have `| None` so that LSP does not require passing them when
+    # instantiating the object.
+    @root_validator
     def check_paths_exist(cls, values):
         if not values['fbx'].is_file():
-            raise ValueError('Path to FBX file does not exist.')
+            raise ValueError(f'Path to FBX file does not exist at {values["fbx"]}')
 
         if not values['gltf'].is_file():
-            raise ValueError('Path to glTF file does not exist.')
+            raise ValueError(f'Path to glTF file does not exist at {values["gltf"]}')
 
         return values
 
@@ -57,10 +73,27 @@ class AvatarIngestData(BaseModel):
     input_path: str
     output_path: str # directory
     mapping: AvatarMapping
+    mapping_path: Path
 
-    @property
-    def mapping_path(self) -> Path:
-        return Path('/mothership3/projects/crs/global/mapping', self.mapping, '.csv')
+    # @validator('mapping_path', pre=True)
+    # def generate_mapping_path(cls, v)
+    @root_validator(pre=True)
+    def generate_mapping_path(cls, values):
+        try:
+            values.update(
+                {'mapping_path': Path(
+                    '/mothership3/projects/crs/global/mapping',
+                    values['mapping'] + '.csv')
+                })
+        except KeyError:
+            raise ValueError('A name for joint mapping table must be provided.')
+        return values
+
+    @validator('mapping_path')
+    def check_mapping_file_exists(cls, v):
+        if not v.is_file():
+            raise ValueError(f'Joint mapping table file does not exist at {v}')
+        return v
 
 class AvatarIngestRequest(BaseModel):
     config: AvatarIngestConfig
@@ -74,12 +107,23 @@ class AvatarIngestSuccess(BaseModel):
     thumbnail: Path | None
 
     @root_validator(pre=True)
+    def interp_paths(cls, values):
+        try:
+            values.update({'gltf': Path(values['request'].data.output_path,
+                                        'ingest.glb'),
+                           'thumbnail': Path(values['request'].data.output_path, 'thumbnail.png')
+                           })
+        except Exception as e:
+            raise e
+        return values
+
+    @root_validator
     def check_paths_exist(cls, values):
         if not values['thumbnail'].is_file():
-            raise ValueError('Path to avatar thumbnail does not exist.')
+            raise ValueError(f'Path to avatar thumbnail does not exist at {values["thumbnail"]}')
 
         if not values['gltf'].is_file():
-            raise ValueError('Path to glTF file does not exist.')
+            raise ValueError(f'Path to glTF file does not exist at {values["gltf"]}')
 
         return values
 
