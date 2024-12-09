0b55399607184f1d5e9e0ac6c45bcfcf8e12d17d
Kalin Petkov <kalinpetkov314@gmail.com>
2024-12-09 20:29:59 +0200

    Updated to match Houdini interface spec at 743857c in cairos-docs

--- cairos_types/houdini.py
+++ cairos_types/houdini.py
@@ -16,22 +16,24 @@ class SequencerConfig(BaseHoudiniConfig):
     # since we have a single hip file, that will be used for several operations
     # a node graph prefix is handy
     prefix: str = "/obj/sequencer"
-    data_input_node: str = f"{prefix}/sequencer/input_data"
+    data_input_node: str = f"{prefix}/sequencer/RPC_DATA_COMES_HERE"
     render_top_node: str = f"{prefix}/output"
 
 class SequencerAvatarData(BaseModel):
-    filepath: Path
+    input: Path
     output_bgeo: Path
     output_gltf: Path
 
-    @validator('filepath')
+    @validator('input')
     def check_avatar_filepath_exists(cls, v):
         if not v.is_file():
             raise ValueError(f'Avatar for sequencing job does not exist at {v}')
+        return v
 
 class SequencerDataWrapper(BaseHoudiniData):
     avatar: SequencerAvatarData
-    animations: list
+    animations: dict[str, list | str | int | float]
+
 
 class SequencerRequest(BaseModel):
     job_id: tuple[str, UUID]
@@ -40,29 +42,29 @@ class SequencerRequest(BaseModel):
 
 class SequencerSuccess(BaseModel):
     request: SequencerRequest
-    # these fields will be computed from the info in request...
-    fbx: Path | None = Field(...)
-    gltf: Path | None = Field(...)
 
-    # ...using a `pre=True` root_validator
-    @root_validator(pre=True)
-    def interp_paths(cls, values):
-        try:
-            values.update({'fbx': Path(values['request'].data.avatar.output, 'output.fbx'),
-                           'gltf': Path(values['request'].data.avatar.output,
-                                        'output-gltf-char.glb')
-                           })
-        except Exception as e:
-            raise e
-        return values
+    bgeo: Path
+    gltf: Path
+
+
+    # @root_validator(pre=True)
+    # def interp_paths(cls, values):
+    #     try:
+    #         values.update({'fbx': Path(values['request'].data.avatar.output, 'output.fbx'),
+    #                        'gltf': Path(values['request'].data.avatar.output,
+    #                                     'output-gltf-char.glb')
+    #                        })
+    #     except Exception as e:
+    #         raise e
+    #     return values
 
     # Finally we check if they are existing files on mothership3. The type
     # annotations have `| None` so that LSP does not require passing them when
     # instantiating the object.
     @root_validator
     def check_paths_exist(cls, values):
-        if not values['fbx'].is_file():
-            raise ValueError(f'Path to FBX file does not exist at {values["fbx"]}')
+        if not values['bgeo'].is_file():
+            raise ValueError(f'Path to BGEO file does not exist at {values["bgeo"]}')
 
         if not values['gltf'].is_file():
             raise ValueError(f'Path to glTF file does not exist at {values["gltf"]}')
@@ -78,47 +80,63 @@ class AvatarIngestConfig(BaseHoudiniConfig):
 AvatarMapping: TypeAlias = Literal['mixamo'] # more to come in the near future
 
 class AvatarIngestData(BaseModel):
-    input_path: Path
-    output_path: Path # directory, ensured in validator
-    mapping: Path
+    input_avatar: Path
+    input_mapping: Path
+    output_bgeo: Path
+    output_gltf: Path
+    output_thumbnail: Path
+    output_skelref: Path
 
     @root_validator(pre=True)
     def generate_mapping_path(cls, values):
         try:
-            m = values['mapping']
+            m = values['input_mapping']
             # `get_args` is a hack to get generic parameters of type, types with
             # generic parameters cannot be used in `isinstance()` and `issubclass()`
             if m in get_args(AvatarMapping):
-                values['mapping'] = Path(
+                values['input_mapping'] = Path(
                     '/mothership3/projects/crs/global/mapping',
                     m + '.csv')
             elif not isinstance(m, Path):
-                values['mapping'] = Path(m)
+                values['input_mapping'] = Path(m)
         except KeyError:
             raise ValueError('A name for joint mapping table must be provided.')
         return values
 
     @root_validator
     def check_files_exist(cls, values):
-        if not values['input_path'].is_file():
-            raise ValueError(f'Input file does not exist at {values["input_path"]}')
-        if not values['output_path'].is_dir():
-            raise ValueError(f'Output path should be a directory, received {values["output_path"]}')
-        if not values['mapping'].is_file(): # TODO add check if the prefix of
+        if not values['input_avatar'].is_file():
+            raise ValueError(f'Input file does not exist at {values["input_avatar"]}')
+
+        if not values['input_mapping'].is_file(): # TODO add check if the prefix of
                                             # the path is indeed
                                             # '/mothership3/projects/crs/global/mapping'
-            raise ValueError(f'Joint mapping table file does not exist at {values["mapping"]}')
+            raise ValueError(f'Joint mapping table file does not exist at {values["input_mapping"]}')
+        if not values['output_bgeo'].suffix == '.bgeo':
+            raise ValueError(f'output_bgeo field should be a path to a file with `.bgeo` extension.')
+        if not values['output_gltf'].suffix == '.gltf':
+            raise ValueError(f'output_gltf field should be a path to a file with `.gltf` extension.')
+        if not values['output_thumbnail'].suffix == '.png':
+            raise ValueError(f'output_thumbnail field should be a path to a file with `.png` extension.')
+        if not values['output_skelref'].suffix == '.png':
+            raise ValueError(f'output_skelref field should be a path to a file with `.png` extension.')
         return values
 
     # overriding init to correctly hint acceptable types for mapping and play
     # nice with the LSP
-    def __init__(self, input_path: Path, output_path: Path, mapping: AvatarMapping | Path):
+    def __init__(self,
+                 input_avatar: Path,
+                 input_mapping: AvatarMapping | Path,
+                 output_bgeo: Path,
+                 output_gltf: Path,
+                 output_thumbnail: Path,
+                 output_skelref: Path):
         d = locals()
         d.pop('self')
         super().__init__(**d)
 
 class AvatarIngestDataWrapper(BaseHoudiniData):
-    avatar: AvatarIngestData # TODO THIS IS NOT THE CORRECT ATTRIBUTE NAME, AS
+    file_paths: AvatarIngestData # TODO THIS IS NOT THE CORRECT ATTRIBUTE NAME, AS
 
 class AvatarIngestRequest(BaseModel):
     config: AvatarIngestConfig
