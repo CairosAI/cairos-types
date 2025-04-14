5da2d6e628442f2f8c0bf234aebfd34ee105e7d6
Kalin Petkov <kalinpetkov314@gmail.com>
2025-04-14 18:44:42 +0300

    Updated Motion class with shot description, creation date

--- cairos_types/core.py
+++ cairos_types/core.py
@@ -1,28 +1,30 @@
 from pydantic.v1 import BaseModel, root_validator
+from datetime import datetime
 import os
 
 class Motion(BaseModel):
     sg_id: int
     description: str
     input: str # filepath of the motion
+    created_at: datetime
+    shot_description: str
 
-    @root_validator
+    @root_validator(pre=True)
     def check_motion(cls, values):
         sg_id = values.get('sg_id')
         description = values.get('description')
         inputfile = values.get('input')
-
+        shot_description = values.get('shot_description')
         # since a ShotGrid version always has an ID this is the least probable
         # case. Therefore we can use the sg_id to identify the motion in the
         # error messages
         if sg_id is None:
-            raise ValueError(f'Motion missing id.')
+            raise ValueError('Motion missing id.')
         elif not isinstance(sg_id, int):
             raise ValueError(f'Motion with id {sg_id} should have an integer id.')
 
         if description is None or len(description) == 0:
             raise ValueError(f'Motion with id {sg_id} has missing or empty name.')
-
         if not os.path.isfile(inputfile):
             raise ValueError(f'Motion with id {sg_id} cannot be found at path {inputfile}')
 
